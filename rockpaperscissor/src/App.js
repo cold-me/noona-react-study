fbe3bf1366d3b3958ee0f77fd625fbeae644001a
coldme <radiantbeautykim@gmail.com>
2025-04-02 00:45:14 +0900

    refact : change history state to object and add updateHistory func
    git commit --amend #

--- rockpaperscissor/src/App.js
+++ rockpaperscissor/src/App.js
@@ -1,12 +1,6 @@
 import { useState } from 'react';
 import './App.css';
 import Box from './components/Box';
-// 1. 박스 2개 생성(타이틀, 사진, 결과)
-// 2. 가위 버위 보 버튼이 있다.
-// 3. 버튼을 클릭하면 클릭한 값이 박스에 보임
-// 4. 컴퓨터는 랜덤하게 아이템 선택이 된다.
-// 5. 3, 4의 결과를 가지고 누가 이겼는데 승패를 따진다.
-// 6. 승패 결과에 따라 테두리 색이바뀐다. (이기면-초록, 지면-빨강, 비기면 -검은색)
 
 const choice = {
     rock: {
@@ -27,7 +21,12 @@ function App() {
     const [computerSelect, setComputerSelect] = useState(null);
     const [userResult, setUserResult] = useState('');
     const [computerResult, setComputerResult] = useState('');
-    const [history, setHistory] = useState([0, 0, 0, 0]);
+    const [history, setHistory] = useState({
+        youWin: 0,
+        computerWin: 0,
+        tie: 0,
+        total: 0,
+    });
     const historyArray = ['You Win:', 'Computer Win:', 'Tie:', 'Total:'];
     const play = (userChoice) => {
         setUserSelect(choice[userChoice]);
@@ -35,23 +34,28 @@ function App() {
         setComputerSelect(computerChoice);
         let result = judgement(userChoice, computerChoice.name);
         setUserResult(result);
-        if (result === 'tie') {
-            setComputerResult(result);
-            setHistory([history[0], history[1], history[2] + 1, history[3] + 1]);
-        } else if (result === 'win') {
-            setComputerResult('lose');
-            setHistory([history[0] + 1, history[1], history[2], history[3] + 1]);
-        } else {
-            setComputerResult('win');
-            setHistory([history[0], history[1] + 1, history[2], history[3] + 1]);
-        }
+        setComputerResult(result === 'tie' ? result : result === 'win' ? 'lose' : 'win');
+        updateHistory(result);
     };
+
+    const updateHistory = (result) => {
+        setHistory((preHistory) => {
+            const newHistory = { ...preHistory };
+            if (result === 'tie') newHistory.tie += 1;
+            else if (result === 'win') newHistory.youWin += 1;
+            else newHistory.computerWin += 1;
+            newHistory.total += 1;
+            return newHistory;
+        });
+    };
+
     const randomChoice = () => {
         let itemArray = Object.keys(choice);
         const randomItem = Math.floor(Math.random() * itemArray.length);
         let final = itemArray[randomItem];
         return choice[final];
     };
+
     const judgement = (user, computer) => {
         console.log(`user, computer : ${user}, ${computer}`);
         if (user === computer) return 'tie';
@@ -59,6 +63,7 @@ function App() {
         else if (user === 'scissors') return computer === 'rock' ? 'lose' : 'win';
         else return computer === 'rock' ? 'win' : 'lose';
     };
+
     return (
         <div className='main-vertical'>
             <div>
@@ -74,9 +79,9 @@ function App() {
                     <button onClick={() => play('paper')}>보</button>
                 </div>
                 <h1>
-                    {history.map((item, i) => (
+                    {historyArray.map((item, i) => (
                         <li key={i}>
-                            {historyArray[i]} {item}
+                            {item} {Object.values(history)[i]}
                         </li>
                     ))}
                 </h1>
